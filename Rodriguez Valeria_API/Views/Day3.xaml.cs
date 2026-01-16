f9f1c04a01a9f1ab01cb47eae0e5207c5bd04e61
SLAB55 <SLAB55@KM101-12-M>
2026-01-16 11:54:08 +0100

    v2

---
+++ Rodriguez Valeria_API/Views/Day3.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rodriguez_Valeria_API.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour Day3.xaml
+    /// </summary>
+    public partial class Day3 : Page
+    {
+        public Day3()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Rodriguez Valeria_API/Views/Today.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Rodriguez_Valeria_API.Views
+{
+    /// <summary>
+    /// Logique d'interaction pour Today.xaml
+    /// </summary>
+    public partial class Today : Page
+    {
+        public Today()
+        {
+            InitializeComponent();
+        }
+    }
+}
